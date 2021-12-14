52b20742a1c95aaa85aabd292177c164f537145d
Amerr <72531524+Fckeveryone@users.noreply.github.com>
2021-12-14 17:05:17 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="Jquery.min.js"></scipt>
+<script src="instascan.min.js"></scipt>
+
+<body>
+<div class="container">
+<div class="row justify-content-center mt-t">
+<div class="col-md-5">
+<div class="card-header bg-transparent mb-0"><h5 class"text-center"><span class="font-weight-bold text-primary">scan</span></h5></div>
+<div class="card-body">
+<video id="preview" width="300" height="300"></video>
+<div class="form-group">
+<input type="text" class="form-control"/>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</body>
